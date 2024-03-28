faddb452bb9d016ba24485e90f608d027a4a4170
Alin <alintrif1405@gmail.com>
2024-03-28 11:33:56 +0200

    hg

---
+++ src/main/java/com/example/springinternship1/SpringInternship1Application.java
@@ -0,0 +1,13 @@
+package com.example.springinternship1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInternship1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInternship1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springinternship1/SpringInternship1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinternship1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInternship1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
